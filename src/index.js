9a6782e95b916a1d242718343d34c0e8ddbd78f1
unknown <nmaaravind@gmail.com>
2023-02-04 10:54:10 +0530

    Set up the server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express=require("express")
+const {PORT}=require("./config/serverConfig")
+const app=express();
+const prepareAndStartServer=()=>{
+    app.listen(PORT,()=>{
+        console.log("Started server in",PORT)
+    })
+}
+prepareAndStartServer();
